91d4899e23fa08e236c1299176bd028fe0ca5b54
Kim Rune Møller <kimrune@hotmail.no>
2025-01-12 21:33:47 +0100

    fetch projects from api

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,34 @@ import React from "react";
 import Card from "../components/Card";
 import Link from "next/link";
 
-export default function page() {
+interface Project {
+  id: number;
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  image: string;
+}
+
+interface ProjectData {
+  data: Project[];
+}
+
+async function fetchProjects() {
+  const res = await fetch("http://localhost/api/v1/projects", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${process.env.BACKEND_TOKEN}`
+    }
+  });
+  const data: ProjectData = await res.json();
+  return data;
+}
+
+export default async function ProjectsPage() {
+  const data = await fetchProjects();
+
   return (
     <main className="bg-[#0C1423]">
       <section className="py-24 px-8 max-w-7xl mx-auto">
@@ -11,118 +38,17 @@ export default function page() {
           Jeg har jobbet med en rekke prosjekter, både for kunder og egne ideer.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12 bg-about">
-          <Card title="Denne websiden">
-            <p className="my-4 ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-              dolores unde, beatae dolore ratione sed deleniti eum, omnis iste
-              quos illum totam molestiae quisquam eius perspiciatis vero magnam
-              ipsum! Quibusdam?
-            </p>
-            <a
-              href="/about"
-              className="underline hover:text-neutral-300 hover:bg-neutral-900"
-            >
-              Les om teknologien bak denne siden
-            </a>
-          </Card>
-          <Card title="Denne websiden">
-            <p className="my-4 ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-              dolores unde, beatae dolore ratione sed deleniti eum, omnis iste
-              quos illum totam molestiae quisquam eius perspiciatis vero magnam
-              ipsum! Quibusdam?
-            </p>
-            <a
-              href="/about"
-              className="underline hover:text-neutral-300 hover:bg-neutral-900"
-            >
-              Les om teknologien bak denne siden
-            </a>
-          </Card>
-          <Card title="Denne websiden">
-            <p className="my-4 ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-              dolores unde, beatae dolore ratione sed deleniti eum, omnis iste
-              quos illum totam molestiae quisquam eius perspiciatis vero magnam
-              ipsum! Quibusdam?
-            </p>
-            <a
-              href="/about"
-              className="underline hover:text-neutral-300 hover:bg-neutral-900"
-            >
-              Les om teknologien bak denne siden
-            </a>
-          </Card>
-          <Card title="Denne websiden">
-            <p className="my-4 ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-              dolores unde, beatae dolore ratione sed deleniti eum, omnis iste
-              quos illum totam molestiae quisquam eius perspiciatis vero magnam
-              ipsum! Quibusdam?
-            </p>
-            <a
-              href="/about"
-              className="underline hover:text-neutral-300 hover:bg-neutral-900"
-            >
-              Les om teknologien bak denne siden
-            </a>
-          </Card>
-          <Card title="Denne websiden">
-            <p className="my-4 ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-              dolores unde, beatae dolore ratione sed deleniti eum, omnis iste
-              quos illum totam molestiae quisquam eius perspiciatis vero magnam
-              ipsum! Quibusdam?
-            </p>
-            <a
-              href="/about"
-              className="underline hover:text-neutral-300 hover:bg-neutral-900"
-            >
-              Les om teknologien bak denne siden
-            </a>
-          </Card>
-          <Card title="Denne websiden">
-            <p className="my-4 ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-              dolores unde, beatae dolore ratione sed deleniti eum, omnis iste
-              quos illum totam molestiae quisquam eius perspiciatis vero magnam
-              ipsum! Quibusdam?
-            </p>
-            <a
-              href="/about"
-              className="underline hover:text-neutral-300 hover:bg-neutral-900"
-            >
-              Les om teknologien bak denne siden
-            </a>
-          </Card>
-          <Card title="Denne websiden">
-            <p className="my-4 ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-              dolores unde, beatae dolore ratione sed deleniti eum, omnis iste
-              quos illum totam molestiae quisquam eius perspiciatis vero magnam
-              ipsum! Quibusdam?
-            </p>
-            <a
-              href="/about"
-              className="underline hover:text-neutral-300 hover:bg-neutral-900"
-            >
-              Les om teknologien bak denne siden
-            </a>
-          </Card>
-          <Card title="Denne websiden">
-            <p className="my-4">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-              dolores unde, beatae dolore ratione sed deleniti eum, omnis iste
-              quos illum totam molestiae quisquam eius perspiciatis vero magnam
-              ipsum! Quibusdam?
-            </p>
-            <a
-              href="/about"
-              className="underline hover:text-neutral-300 hover:bg-neutral-900"
-            >
-              Les om teknologien bak denne siden
-            </a>
-          </Card>
+          {data.data.map((project) => (
+            <Card key={project.id} title={project.title} image={project.image}>
+              <p className="mb-4">{project.description}</p>
+              <Link
+                href={`/projects/${project.slug}`}
+                className="underline hover:text-neutral-300 hover:bg-neutral-900"
+              >
+                Les om teknologien bak denne siden
+              </Link>
+            </Card>
+          ))}
         </div>
       </section>
       <section
