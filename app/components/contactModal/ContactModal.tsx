f561404b31a7b9d36780ff73d42c34c5e24725fc
Kim Rune Møller <kimrune@hotmail.no>
2025-01-13 02:35:46 +0100

    open AI integration

--- app/components/contactModal/ContactModal.tsx
+++ app/components/contactModal/ContactModal.tsx
@@ -1,45 +1,57 @@
+import { prompt } from "@/app/actions";
+import { FormEvent, useEffect, useState } from "react";
+import useContactModalStore from "@/app/store/useContactModalStore";
+
 interface ModalProps {
+  userQuestion: string;
   onClose: () => void;
 }
 
 export default function ContactModal({ onClose }: ModalProps) {
+  const [promptResponse, setPromptResponse] = useState("");
+  const userQuestion = useContactModalStore((state) => state.userQuestion);
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+  };
+  useEffect(() => {
+    if (promptResponse != "") {
+      return;
+    }
+    if (userQuestion === "") {
+      return;
+    }
+    const getPrompt = async () => {
+      const response = await prompt(userQuestion);
+      setPromptResponse(response);
+    };
+    getPrompt();
+  }, [userQuestion, promptResponse]);
   return (
     <div className="fixed inset-0 z-50 bg-black bg-opacity-50">
-      <div className="bg-white w-1/2 h-1/2 mx-auto my-10 rounded-3xl p-8">
-        <h2 className="text-2xl font-bold">Kontakt meg</h2>
-        <form className="mt-4 space-y-4">
-          <label htmlFor="name" className="block">
-            Navn
-            <input
-              type="text"
-              id="name"
-              className="w-full bg-gray-100 rounded-3xl p-2"
-            />
-          </label>
-          <label htmlFor="email" className="block">
-            E-post
-            <input
-              type="email"
-              id="email"
-              className="w-full bg-gray-100 rounded-3xl p-2"
-            />
-          </label>
+      <div className="bg-white w-full max-h-[600px] overflow-scroll sm:w-96 mr-1 mx-auto rounded-3xl p-8 mb-4 absolute bottom-0 right-0 text-neutral-800">
+        <h2 className="text-xl font-bold">{userQuestion}</h2>
+        <p className="mt-2 text-green-900 whitespace-pre-wrap">
+          {promptResponse}
+        </p>
+        <form onSubmit={handleSubmit} className="mt-4 space-y-4">
           <label htmlFor="message" className="block">
-            Melding
+            Svar så godt du kan og trykk videre
             <textarea
               id="message"
               className="w-full bg-gray-100 rounded-3xl p-2"
             ></textarea>
           </label>
-          <button
-            type="submit"
-            className="bg-black text-white rounded-3xl p-2 w-full"
-          >
-            Send
-          </button>
-          <button onClick={onClose} className="text-black underline">
-            Lukk
-          </button>
+          <div className="flex items-center space-x-4">
+            <button
+              type="submit"
+              className="bg-black text-white rounded-3xl p-2 w-full"
+            >
+              Videre
+            </button>
+            <button onClick={onClose} className="text-black underline">
+              Lukk
+            </button>
+          </div>
         </form>
       </div>
     </div>
