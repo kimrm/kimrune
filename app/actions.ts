cddfb56ee58b64d8922e581475c10c7d6c1bcdfc
Kim Rune Møller <kimrune@hotmail.no>
2025-01-13 02:35:08 +0100

    open AI integration

--- app/actions.ts
+++ app/actions.ts
@@ -39,3 +39,41 @@ export async function sendContactRequest(
     message: ""
   };
 }
+
+export async function prompt(userQuestion: string) {
+  if (!userQuestion) {
+    return false;
+  }
+
+  const messages = [
+    {
+      role: "system",
+      content:
+        "Du jobber i salgsmottaket og skal hjelpe kunden med å finne ut hva de ønsker hjelp til. Spør kunden om hva de ønsker hjelp til, og list gjerne opp punkter som kan være viktig å få svar på for å kunne gi en løsning."
+    },
+    {
+      role: "user",
+      content: userQuestion
+    }
+  ];
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        model: "gpt-4o-mini",
+        messages: messages,
+        max_tokens: 500
+      })
+    });
+    const answer = await response.json();
+
+    return answer.choices[0].message.content;
+  } catch (error) {
+    console.log("error ", error);
+  }
+}
