84cedc7c4f8b64688e69618c07a2079be89b0297
Kim Rune Møller <kimrune@hotmail.no>
2025-01-17 10:52:26 +0100

    import Pacifico font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import Header from "./components/header/Header";
+import ModalContainer from "./components/modalContainer/ModalContainer";
 import { Afacad } from "next/font/google";
+import { Pacifico } from "next/font/google";
 import "./globals.css";
 
 const afacadSans = Afacad({
@@ -8,8 +10,14 @@ const afacadSans = Afacad({
   subsets: ["latin"]
 });
 
+const pacifico = Pacifico({
+  variable: "--font-pacifico",
+  subsets: ["latin"],
+  weight: "400"
+});
+
 export const metadata: Metadata = {
-  title: "kodello",
+  title: "Kodello",
   description: "Webutvikler"
 };
 
@@ -20,12 +28,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${afacadSans.variable} antialiased`}>
+      <body
+        className={`${afacadSans.variable} ${pacifico.variable} antialiased`}
+      >
         <Header />
         {children}
         <footer className="pt-24 pb-8 px-8 text-center bg-[#0C1423]">
           <p>&copy; {new Date().getFullYear()} kodello - K-R Møller</p>
         </footer>
+        <ModalContainer />
       </body>
     </html>
   );
