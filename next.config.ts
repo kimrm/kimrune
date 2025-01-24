eb94145078010492486caad8638dbff98bc60c0f
Kim Rune Møller <kimrune@hotmail.no>
2025-01-24 10:29:07 +0100

    config images for domains and remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "80",
+        pathname: "/storage/images/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
